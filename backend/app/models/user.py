5aaf1de8f8c64c4ecca781f83ab0376841d7631f
Paras <Paraskes14@gmail.comm>
2026-07-20 19:17:50 +0530

    Configure Alembic database migrations

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,44 +1,49 @@
-from sqlalchemy import Column
-from sqlalchemy import String
-from sqlalchemy import DateTime
-from sqlalchemy import Boolean
-
 import uuid
 
+from sqlalchemy import Boolean
+from sqlalchemy import DateTime
+from sqlalchemy import String
+from sqlalchemy.sql import func
+
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
 from app.database.base import Base
+
 
 class User(Base):
 
     __tablename__ = "users"
 
-    id = Column(
+    id: Mapped[str] = mapped_column(
         String,
         primary_key=True,
         default=lambda: str(uuid.uuid4())
     )
 
-    username = Column(
-        String,
+    username: Mapped[str] = mapped_column(
+        String(50),
         unique=True,
         nullable=False
     )
 
-    email = Column(
-        String,
+    email: Mapped[str] = mapped_column(
+        String(100),
         unique=True,
         nullable=False
     )
 
-    password = Column(
-        String,
+    password: Mapped[str] = mapped_column(
+        String(255),
         nullable=False
     )
 
-    is_active = Column(
+    is_active: Mapped[bool] = mapped_column(
         Boolean,
         default=True
     )
 
-    created_at = Column(
-        DateTime
+    created_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now()
     )
